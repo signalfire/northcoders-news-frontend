c4b94cf92e6d3f72b9189563fe8408f0aed21bba
Robert Coster <rob@signalfire.co.uk>
2018-09-20 07:13:39 +0100

    Added css in js for notfound component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,13 +1,26 @@
 import React, {Fragment} from 'react';
 import Typography from '@material-ui/core/Typography';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 
-const NotFound = () => {
+const styles = {
+    title: {
+        marginBottom:'2rem'
+    }
+}
+
+const NotFound = (props) => {
+    const {classes} = props;
     return (
         <Fragment>
-            <Typography component="h1" variant="display1">Sorry...</Typography>
+            <Typography component="h1" variant="display1" className={classes.title}>Sorry...</Typography>
             <Typography component="p">Unable to find the content requested</Typography>
         </Fragment>
     );
 };
 
-export default NotFound;
+NotFound.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(NotFound);
