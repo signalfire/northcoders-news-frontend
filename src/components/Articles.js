bc0786dd3030ee10746ed7ccb2fbc0354b32b593
Robert Coster <rob@signalfire.co.uk>
2018-09-20 08:58:02 +0100

    Cleaned up imports for articles

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import Avatar from '@material-ui/core/Avatar';
-import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
-import Grid from '@material-ui/core/Grid';
+
+import {Card, CardContent, Typography, Avatar, Grid} from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 
 import moment from 'moment';
 import produce from 'immer';
 
-import * as api from '../utils/api';
 import ArticleForm from './ArticleForm';
+import * as api from '../utils/api';
 
 const styles = {
     title: {
