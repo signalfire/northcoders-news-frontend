0f19062414b4583ffc4dff178f9f4f90b297aecf
Robert Coster <rob@signalfire.co.uk>
2018-09-20 08:57:07 +0100

    Cleaned up imports for articleform

--- src/components/ArticleForm.js
+++ src/components/ArticleForm.js
@@ -1,7 +1,8 @@
 import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
 import {Input, Button, Card, CardContent, Typography} from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
-import PropTypes from 'prop-types';
 
 const styles = {
     title: {
